95b6cc562165506e90c8b2d0e25492e612eec0de
KimMinJoon <kimminjoooon@gmail.com>
2022-01-19 18:01:00 +0900

    회원 서비스클래스 생성 및 예외처리

---
+++ resume-spring/src/main/java/resume/resumespring/service/MemberService.java
@@ -0,0 +1,40 @@
+package resume.resumespring.service;
+
+import resume.resumespring.domain.Member;
+import resume.resumespring.repository.MemberRepository;
+import resume.resumespring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     * @param member
+     * @return
+     */
+    public Long join(Member member) {
+        // 같은 이름이 있는 경우 회원가입을 할 수 없다.
+
+        validateDuplicateMember(member);        // 회원 중복 이름 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
