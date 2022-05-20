341992ba4c591275fc54a02e04fdf6973efb59c1
KimMinJoon <kimminjoooon@gmail.com>
2022-05-20 18:18:20 +0900

    17강완료

--- resume-spring/src/main/java/resume/resumespring/controller/MemberController.java
+++ resume-spring/src/main/java/resume/resumespring/controller/MemberController.java
@@ -2,6 +2,8 @@ package resume.resumespring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import resume.resumespring.domain.Member;
 import resume.resumespring.service.MemberService;
 
@@ -15,4 +17,21 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    @GetMapping("/members/new")
+    public String createdForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        System.out.println("member = " + member.getName());
+
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
 }
---
+++ resume-spring/src/main/java/resume/resumespring/controller/MemberForm.java
@@ -0,0 +1,21 @@
+package resume.resumespring.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberForm {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+
+}
